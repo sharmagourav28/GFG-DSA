f8ed478a6e2bfa052ff836bed58e17b8cfb934a6
Gourav sharma <gouravsharma2806@gmail.com>
2023-07-09 12:59:25 +0530

    modified:   Array/Array2.cpp 	new file:   linkedlist/link1.cpp 	new file:   linkedlist/link1.exe 	new file:   linkedlist/tempCodeRunnerFile.cpp

---
+++ linkedlist/link1.cpp
@@ -0,0 +1,46 @@
+#include <iostream>
+using namespace std;
+class Node
+{
+public:
+    int data;
+    Node *next;
+
+    Node(int data)
+    {
+        this->data = data;
+        this->next = NULL;
+    }
+};
+
+void insertAthead(Node *&head, int data)
+{
+    Node *temp = new Node(data);
+    temp->next = head;
+    head = temp;
+}
+
+void printLinkedlist(Node *&head)
+{
+    Node *temp = head;
+    while (temp != NULL)
+    {
+        cout << temp->data << " ";
+        temp = temp->next;
+    }
+    cout << endl;
+}
+int main()
+{
+    // create a new node
+    Node *node1 = new Node(10);
+    // cout << node1->data << endl;
+    // cout << node1->next << endl;
+
+    // head pointed to node1
+    Node *head = node1;
+    printLinkedlist(head);
+    insertAthead(head, 23);
+    insertAthead(head, 15);
+    printLinkedlist(head);
+}
